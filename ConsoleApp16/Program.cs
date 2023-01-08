0da6ded945086a4487f8a4f5fcf1b6deb0f3a89a
rajabi <hamidmrh@yahoo.com.com>
2023-01-08 13:00:28 +0330

    Add project files.

---
+++ ConsoleApp16/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+def calculate_basic_math(num1 , operator, num2): 
+if operator == "":
+result = num1  num2
+elif operator == "":
+result = num1  num2
+elif operator == "":
+result = num1  num2
+elif operator == "": 
+result = num1  num2
+else:
+result = "The operator is not valid!"
+print(result)
+# function call
+try: 
+num1 = int(input("Please Enter Number1: "))
+operator = input("Please Enter the operator(): ")
+num2 = int(input("Please Enter Number2: "))
+except:
+print("Something went wrong!")
+else:
+calculate_basic_math(num1, operator, num2)
+
